5be7145bbea3f39843689d929709aa3ba571eb8e
Matsvei Dzebysh (245919) <245919@student.pwr.edu.pl>
2022-05-26 20:10:55 +0200

    Initial commit

---
+++ src/main/java/com/example/multicinema/MultiCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.multicinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiCinemaApplication.class, args);
+	}
+
+}
