2c57e0e8ee0b25e4e4dad4afa6dfa254bfb0f8ff
Sheinerman <80273352+Sheinerman@users.noreply.github.com>
2022-06-19 21:18:32 +0200

    added data to the database and CrossOrigin

--- src/main/java/com/example/multicinema/controllers/AdmController.java
+++ src/main/java/com/example/multicinema/controllers/AdmController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins={"*"})
 @RestController
 public class AdmController {
+
     @Autowired
     AdmService admService;
     @RequestMapping(value="/admins", method = RequestMethod.GET)
